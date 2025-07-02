61da804eca09761f81e3acb72d3aebf15f72d4ac
Roger <rogerr_machadoo@hotmail.com>
2025-07-01 21:10:07 -0300

    layout responsivo para celular e tablet finalizado

--- scripts.js
+++ scripts.js
@@ -21,3 +21,11 @@ document
       behavior: "smooth",
     });
   });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
